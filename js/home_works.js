3ebd5bcb8b18cfebc06d8fc452d04cb983bebdb8
DanielB008 <danielbakirov4@gmail.com>
2025-04-07 00:43:47 +0600

    first commit

--- js/home_works.js
+++ js/home_works.js
@@ -19,19 +19,83 @@ gmailButton.onclick = () => {
 const parentBlock = document.querySelector('.parent_block')
 const childBlock = document.querySelector('.child_block')
 
-const maxWidth = parentBlock.offsetWidth - childBlock.offsetWidth
+let positionX = 0, positionY = 0
 
-let position = 0
+const maxWidth = parentBlock.clientWidth - childBlock.clientWidth
+const maxHeight = parentBlock.clientHeight - childBlock.clientHeight
+let direction = 1
+
+
+
 
 const count = () => {
-    if (position < maxWidth) {
-        position++
-        childBlock.style.left = `${position}px`
-
-        requestAnimationFrame(count)
+    if (direction === 1) {
+        if (positionX < maxWidth) {
+            positionX++
+            childBlock.style.left = `${positionX}px`
+            requestAnimationFrame(count)
+        } else {
+            direction = 2
+            requestAnimationFrame(count)
+        }
+    } else if (direction === 2) {
+        if (positionY < maxHeight) {
+            positionY++
+            childBlock.style.top = `${positionY}px`
+            requestAnimationFrame(count)
+        } else {
+            direction = 3
+            requestAnimationFrame(count)
+        }
+    } else if (direction === 3) {
+        if (positionX > 0) {
+            positionX--
+            childBlock.style.left = `${positionX}px`
+            requestAnimationFrame(count)
+        } else {
+            direction = 4
+            requestAnimationFrame(count)
+        }
+    } else if (direction === 4) {
+        if (positionY > 0) {
+            positionY--
+            childBlock.style.top = `${positionY}px`
+            requestAnimationFrame(count)
+        } else {
+            direction = 1
+            requestAnimationFrame(count)
+        }
     }
 }
 
 count()
 
+// 3
 
+const seconds = document.querySelector('#seconds')
+const start = document.querySelector('#start')
+const stop = document.querySelector('#stop')
+const reset = document.querySelector('#reset')
+
+let counter = 0
+
+
+function display() {
+    seconds.textContent = counter;
+}
+
+start.addEventListener('click', () => {
+    interval = setInterval(() => {
+        counter++
+        display()
+    },500)
+})
+
+stop.addEventListener('click', () => {
+    clear = clearInterval(interval)
+})
+
+reset.addEventListener('click', () => {
+    counter = 0
+    display()
+})
