661ef7b9973a6abe5334ad07194e06490f57feae
DanielB008 <danielbakirov4@gmail.com>
2025-04-13 22:36:41 +0600

    third commit

--- js/home_works.js
+++ js/home_works.js
@@ -99,3 +99,65 @@ reset.addEventListener('click', () => {
     counter = 0
     display()
 })
+
+//4
+
+const charactersList = document.querySelector('.characters-list');
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET', '../data/characters.json');
+
+xhr.onload = function() {
+    if (xhr.status === 200) {
+        const persons = JSON.parse(xhr.response);
+        persons.forEach(person => {
+            const characterCard = document.createElement('div');
+            characterCard.className = 'character-card';
+
+            const characterPhoto = document.createElement('div');
+            characterPhoto.className = 'character-photo';
+
+            const characterPhotoImg = document.createElement('img');
+            characterPhotoImg.src = person.photo;
+            characterPhotoImg.alt = person.name;
+
+            characterPhoto.appendChild(characterPhotoImg);
+
+            characterCard.appendChild(characterPhoto);
+
+            const h3 = document.createElement('h3');
+            h3.innerHTML = person.name;
+            characterCard.appendChild(h3);
+
+
+            const age = document.createElement('p');
+            age.innerHTML = `Age: ${person.age}`;
+            characterCard.appendChild(age);
+
+
+
+            charactersList.appendChild(characterCard);
+        });
+    } else {
+        console.log('Ошибка при загрузке персонажей');
+    }
+};
+
+xhr.send();
+
+// 5
+
+xhr2 = new XMLHttpRequest();
+xhr2.open('GET', '../data/any.json')
+xhr2.setRequestHeader('Content-Type', 'application/json');
+
+
+xhr2.onload = function() {
+    if (xhr2.status === 200) {
+        const info = JSON.parse(xhr2.response);
+        console.log(info);
+    }
+}
+
+
+xhr2.send()
